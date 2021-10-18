0a9c2b9c118052bfc8e737a049860121b264fc05
Bill-Zuckuberg <thanh821329@gmail.com>
2021-10-19 00:36:12 +0700

    added RadioGradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
-// import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -25,8 +24,30 @@ class MyApp extends StatelessWidget {
           Container(
             height: 300.0,
             decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-          )
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.1, 0.5, 0.9],
+                    colors: [Colors.yellow, Colors.indigo, Colors.teal])),
+            child: const Text(
+              "LinearGradient",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const Divider(),
+          Container(
+            height: 300,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: RadialGradient(
+                    radius: 0.7, colors: [Colors.blue, Colors.red])),
+            child: const Text(
+              "RadioGradient",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const Divider()
         ],
       ),
     );
